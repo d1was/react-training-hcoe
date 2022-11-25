ff64789839eb7e4d09ce0c3674cb9f852d1bc29c
Diwas KC <diwas@frisgo.com>
2022-11-25 08:52:04 +0545

    weather app complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [notifications, setNotifications] = useState(0);
+
+  useEffect(() => {
+    if (notifications > 0) {
+      document.title = "You have " + notifications + " notifications";
+    }
+  })
+
+  function sendNotification() {
+    setNotifications(notifications + 1);
+  }
+
   return (
     <div className="App">
+      <button onClick={sendNotification}>Send Notification</button>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import AnimeQuotes from './components/AnimeQuotes'
+import Weather from './components/Weather'
 import './index.css'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <Weather />
+    </div>
   </React.StrictMode>
 )
