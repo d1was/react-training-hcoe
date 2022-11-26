977b13fe89881ca978b5de99b784aa58ff30bc63
Diwas KC <diwas@frisgo.com>
2022-11-26 09:36:56 +0545

    react router intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import './App.css'
+import { Link, Outlet, Route, Routes } from "react-router-dom";
 import Game from "./components/Game";
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Game />
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/game">Game</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/posts" element={<Post />} />
+          <Route path="/game" element={<Game />} />
+        </Route>
+
+        <Route path="/game" element={<Game />} />
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <div>Header</div>
+      <div>sidebar</div>
+      <Outlet />
+      <div>footer</div>
+    </>
+
   )
 }
 
-export default App
+function Post() {
+  return (
+    <div>My posts</div>
+  )
+}
